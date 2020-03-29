c6a7cf1554ff03bb781affe2299814c7942d7ce3
Ptrip9199 <poojiltripathi@gmail.com>
2020-03-29 10:28:09 +0000

    modified:   proj/back/schema.py 	deleted:    proj/mock/schema.js 	deleted:    proj/mock/try.js

--- proj/back/schema.py
+++ proj/back/schema.py
@@ -1,5 +1,4 @@
 import graphene
-from graphene import *
 from graphene.relay import Node
 from graphene_mongo import MongoengineConnectionField, MongoengineObjectType
 from models import Patient as PatientModel
@@ -8,41 +7,52 @@ from models import Visits as VisitModel
 from database import conn
 
 conn()
-
+#classes for Queries
 class Patients (MongoengineObjectType):
     class Meta:
         model = PatientModel
         interfaces = (Node,)
-
 
 class Visits(MongoengineObjectType):
     class Meta:
         model = VisitModel
         interfaces = (Node,)
 
-
-class CreatePatient(graphene.Mutation):
-    """docstring for CreatePatients"""
-    class Argumenrs:
-            fName = graphene.String()
-            lMame = graphene.String()
-
-    ok = graphene.Boolean()
-    patient = graphene.Field(lambda : Patients)
-
-    def mutate(root, info , fName,lName):
-        pat = PatientModel(f_name=fName , l_name=lName )
-        pat.save()
-        return CreatePatient(patient = pat)
-
-
-
+#query class 
 class Query(graphene.ObjectType):
     name = 'Query'
     find_patients = MongoengineConnectionField(Patients)
     find_visits = MongoengineConnectionField(Visits)
 
+#classes for Mutations
 
+class CreatePatient(graphene.Mutation):
+    """docstring for CreatePatients"""
+
+    class Arguments:
+        fname = graphene.String(required=True)
+        lname = graphene.String()
+        DoB = graphene.types.datetime.Date()
+        Parentname = graphene.List(graphene.String)
+        mobilenm = graphene.String()
+        emailid = graphene.String()
+        visitsdone = graphene.List(graphene.String)
+
+    ok = graphene.Boolean()
+    patient = graphene.Field(lambda: Patients)
+
+    def mutate(self,info,fname,lname,DoB,Parentname,mobilenm,emailid,visitsdone=[]):
+        pat = PatientModel()
+        pat.f_name = fname
+        pat.l_name = lname
+        pat.DoB = DoB
+        pat.Parent_name = Parentname
+        pat.mobile_nm = mobilenm
+        pat.email_id = emailid
+        pat.save()
+        return CreatePatient(patient=pat)
+
+#Mutation Class
 class Mutation(graphene.ObjectType):
     """docstring for Mutations"""
     Create_patient = CreatePatient.Field()
--- proj/mock/try.js
+++
@@ -1,3 +0,0 @@
-const start = new Date();
-const dt = start.toISOString()
-console.log(dt)
